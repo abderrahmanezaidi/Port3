a3114cd93c9ba9c0b9a02117d0d6206f728d4a05
abderrahmanezaidi <124533486+abderrahmanezaidi@users.noreply.github.com>
2024-10-21 17:07:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-})
+  server: {
+    host: '0.0.0.0', // Allow access from any IP
+    port: 5173, // You can keep this port, or change it if needed
+  },
+});
